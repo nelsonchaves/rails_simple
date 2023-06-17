5f8d1422d7d14d55568706f4ebc9b0c4b2b6f73d
Nelson Chaves <nelsonchavespro@gmail.com>
2023-06-16 20:02:39 -0700

    add methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def how_old
+    age || "Can't tell"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/my-page", to: "site#index"
+  resources :users
 end
